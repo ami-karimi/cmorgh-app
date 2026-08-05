e6c12e5def6509f433c100b40f9efb42f3dad712
mac <takfashomal@gmail.com>
2026-08-06 02:51:41 +0330

    up

--- routes/web.php
+++ routes/web.php
@@ -88,3 +88,12 @@ Route::middleware(['auth', 'is_admin'])->prefix('admin')->name('admin.')->group(
     Route::get('/users/{id}',\App\Livewire\Admin\UserDetails::class)->name('users.show');
 
 });
+
+Route::prefix('mobile')->group(function () {
+    Route::post('/sign-in', [\App\Http\Controllers\Mobile\AuthController::class, 'sign_in'])->name('sign_in');
+    Route::post('/config', [\App\Http\Controllers\Mobile\AuthController::class, 'is_valid_token'])->name('is_valid_token');
+    Route::post('/servers', [\App\Http\Controllers\Mobile\AuthController::class, 'get_servers'])->name('get_servers');
+    Route::post('/notifications', [\App\Http\Controllers\Mobile\AuthController::class, 'get_notifications'])->name('get_notifications');
+    Route::get('/get-ip', [\App\Http\Controllers\Mobile\AuthController::class, 'get_ip'])->name('get_ip');
+
+});
